daa18a1fc0c8a89365c1d414fd35f5550ff6c56a
Marco Boretto <marco.bore@gmail.com>
2022-12-30 22:27:38 +0100

    QR scan bot init

--- src/App.vue
+++ src/App.vue
@@ -1,153 +1,70 @@
 <template>
   <div id="main">
-    <h3>Window Control</h3>
-    <b>isExpanded</b>: {{ TWA.isExpanded }}
-    <button @click="TWA.expand()">Expand</button>
-    <button @click="TWA.close()">Close</button><br>
+    <div v-if="is_telegram_api_update">
+      <h3>QR scanner</h3>
+      <button @click="showQRScanner()">Scan QR code</button><br>
 
-    <b>isClosingConfirmationEnabled</b>: {{ TWA.isClosingConfirmationEnabled }}
-    <button @click="toggleClosingDialog()">Enable/Disable Confirmation Dialog</button><br>
-
-    <b>viewportHeight</b>: {{ TWA.viewportHeight }} <br>
-    <b>viewportStableHeight</b>: {{ TWA.viewportStableHeight }} <br>
+      <!--<h3>Window Control</h3>
+      <b>isExpanded</b>: {{ TWA.isExpanded }}
+      <button @click="TWA.expand()">Expand</button>
+      <button @click="TWA.close()">Close</button><br>
 
 
-    <h3>Theme colors</h3>
-    <b>colorScheme</b>: {{ TWA.colorScheme }} <br>
-    <b>themeParams</b>:<br>
-    <div v-for="(rgb_color, key) in TWA.themeParams">
-     <label for="head"><b>{{ key }}</b>: {{ rgb_color }}
-       <input type="color" :value="rgb_color" disabled>
-     </label> <br>
+      <h3>Functions and buttons</h3>
+
+      -->
+      <div v-if="code">{{ code }}
+      <button @click="openLink()">{{ code }}</button>
+      <!--<button @click="openLink()">copy to clipboard</button>-->
+      </div>
+
+
+      <h3>Bot API version available</h3>
+      <b>version</b>:  <br>
+      <b>isVersionAtLeast('6.4')</b>: {{  }} <br>
     </div>
 
-    <h3>Web App colors</h3>
-     <label for="header"><b>headerColor</b>: {{ TWA.headerColor }}
-       <input type="color" name="header" :value="TWA.headerColor" disabled><br>
-       <select @input="changeHeaderColor">
-         <option disabled value="">Please select one</option>
-         <option>bg_color</option>
-         <option>secondary_bg_color</option>
-       </select>
-     </label> <br>
-
-     <label for="background"> <b>backgroundColor</b>: {{ TWA.backgroundColor }}
-       <input type="color" name="background" :value="TWA.backgroundColor" disabled><br>
-       <select @input="changeBackgroundColor">
-         <option disabled value="">Please select one</option>
-         <option>bg_color</option>
-         <option>secondary_bg_color</option>
-       </select>
-     </label> <br>
-
-    <h3>Data received</h3>
-    <b>initData</b>: {{ TWA.initData }} <br>
-    <b>initDataUnsafe</b>: <pre>{{ TWA.initDataUnsafe }}</pre><br>
-
-    <h3>Bot API version available</h3>
-    <b>version</b>: {{ TWA.version }} <br>
-    <b>isVersionAtLeast('6.4')</b>: {{ TWA.isVersionAtLeast('6.4') }} <br>
-
-    <h3>Haptic Feedback</h3>
-    <select v-model="style_selected">
-      <option disabled value="">Please select one</option>
-      <option>light</option>
-      <option>medium</option>
-      <option>heavy</option>
-      <option>rigid</option>
-      <option>soft</option>
-    </select>
-    <button @click="hapticImpact()">Haptic Feedback ({{ style_selected }})</button><br>
-
-    <h3>Functions and buttons</h3>
-    <button @click="TWA.openLink('https://github.com/MBoretto/telegram-bot-vue-wep-app')">Open link in an external browser</button><br>
-    <button @click="toggleBackButton()">Show/hide Back Button</button><br>
-    <button @click="toggleMainButton()">Show/hide Main Button</button>
-    <button @click="toggleEnableMainButton()">Enable/Disable Main Button</button>
-    <button @click="toggleProgressMainButton()">Show/Hide Main Button progress</button><br>
-
-    <h3>QR scanner</h3>
-    <button @click="showQRScanner()">Scan QR code</button><br>
-    <h3>Popups</h3>
-    <button @click="TWA.showAlert('Showing an Alert!!')">Show Alert</button><br>
-    <button @click="TWA.showConfirm('Showing confirm message')">Show Confirm</button><br>
-    <button @click="showPopup()">Show Popup message</button><br>
-    <button @click="showPopup2()">Show Popup message2</button><br>
-    <br>
+    <div v-if="!is_telegram_api_update">
+      Please update Telegram to Use the app!
+       Telegram API version needed 6.4 or greater
+      Your Telegram API version: {{ TWA.version }}
+    </div>
   </div>
 </template>
 
 
 <script>
-// Objects
-//BackButton
-//MainButton
-//HapticFeedback
-
-// Methods
-//enableClosingConfirmation() NEW
-//disableClosingConfirmation() NEW
-//onEvent(eventType, eventHandler)
-
-//offEvent(eventType, eventHandler)
-//sendData(data)
-//openTelegramLink(url)
-//openInvoice(url[, callback])
-//showPopup(params[, callback]) NEW
-//showAlert(message[, callback]) NEW
-//showConfirm(message[, callback]) NEW
 export default {
   data() {
     return {
-      style_selected: 'medium',
+      code: null,
+      is_telegram_api_update: false,
     };
   },
   created() {
-    // Binnding function to all the event types
-    this.TWA.onEvent('themeChanged', this.themeChanged);
-    // triggered too often
-    //this.TWA.onEvent('viewportChanged', this.viewportChanged);
-    this.TWA.onEvent('mainButtonClicked', this.mainButtonClicked);
-    this.TWA.onEvent('backButtonClicked', this.backButtonClicked);
-    // I couldn't trigger this yet
-    this.TWA.onEvent('settingsButtonClicked', this.settingsButtonClicked);
-    this.TWA.onEvent('invoiceClosed', this.invoiceClosed);
-    // Seems that the popup is an alter itself
-    // Commenting this otherwise I'm stuck in a loop
-    //this.TWA.onEvent('popupClosed', this.popupClosed);
+    // Binding function to all the event types
+    //this.TWA.onEvent('themeChanged', this.themeChanged);
     this.TWA.onEvent('qrTextReceived', this.processQRCode);
+    this.is_telegram_api_update = this.TWA.isVersionAtLeast('6.4');
   },
   mounted() {
     // What is the best? mounted or created??
     this.TWA.ready();
+    this.showQRScanner()
   },
   methods: {
     // attached with onEvent function during created
     themeChanged() {
-      this.TWA.showAlert('Theme has changed');
+      //this.TWA.showAlert('Theme has changed');
     },
-    viewportChanged() {
-      this.TWA.showAlert('Viewport has changed');
-    },
-    mainButtonClicked() {
-      this.TWA.showAlert('Main button was pressed');
-      window.Telegram.WebApp.showAlert('Main button was pressed version2');
-    },
-    backButtonClicked() {
-      this.TWA.showAlert('back button was clicked');
-    },
-    settingsButtonClicked() {
-      this.TWA.showAlert('Settings button pressed');
-    },
-    invoiceClosed() {
-      this.TWA.showAlert('Invoice was closed');
-    },
-    popupClosed() {
-      this.TWA.showAlert('Popup was closed');
+    openLink() {
+      TWA.openLink(this.code);
     },
     processQRCode(data) {
+       this.hapticImpact();
        this.TWA.closeScanQrPopup();
-       this.TWA.showAlert(data.data);
+       //this.TWA.showAlert(data.data);
+       this.code = data.data;
     },
     // End of callbacks
     showQRScanner() {
@@ -156,82 +73,9 @@ export default {
         };
       this.TWA.showScanQrPopup(par);
     },
-    changeHeaderColor(event) {
-        const color = event.target.value;
-        this.TWA.setHeaderColor(color);
-    },
-    changeBackgroundColor(event) {
-        const color = event.target.value;
-        this.TWA.setBackgroundColor(color);
-    },
-    toggleBackButton() {
-      if (this.TWA.BackButton.isVisible) {
-        this.TWA.BackButton.hide();
-      } else {
-        this.TWA.BackButton.show();
-      }
-    },
-    toggleMainButton() {
-      if (this.TWA.MainButton.isVisible) {
-        this.TWA.MainButton.hide();
-      } else {
-        this.TWA.MainButton.show();
-      }
-    },
-    toggleEnableMainButton() {
-      if (!this.TWA.MainButton.isVisible) {
-        this.TWA.MainButton.show();
-      }
-      if (this.TWA.MainButton.isActive) {
-        this.TWA.MainButton.disable();
-      } else {
-        this.TWA.MainButton.enable();
-      }
-    },
-    toggleProgressMainButton() {
-      if (!this.TWA.MainButton.isVisible) {
-        this.TWA.MainButton.show();
-      }
-      if (this.TWA.MainButton.isActive) {
-        this.TWA.MainButton.hideProgress();
-      } else {
-        this.TWA.MainButton.showProgress();
-      }
-    },
-    toggleClosingDialog() {
-      if (this.TWA.isClosingConfirmationEnabled) {
-        this.TWA.disableClosingConfirmation();
-      } else {
-        this.TWA.enableClosingConfirmation();
-      }
-    },
     hapticImpact() {
-      this.TWA.HapticFeedback.impactOccurred(this.style_selected);
-    },
-    showPopup() {
-      const par = {
-                    title: "Popup title",
-                    message: "Popup with default, ok and close buttons",
-                    buttons: [
-                      {id: "default", type: "default", text: "default"},
-                      {id: "ok", type: "ok", text: "ok"},
-                      {id: "close", type: "close", text: "close"}
-                    ]
-                  };
-
-      this.TWA.showPopup(par);
-    },
-    showPopup2() {
-      const par = {
-                    title: "Popup title",
-                    message: "Popup with cancel and destrucitve buttons",
-                    buttons: [
-                      {id: "cancel", type: "cancel", text: "cancel"},
-                      {id: "destructive", type: "destructive", text: "destructive"}
-                    ]
-                  };
-
-      this.TWA.showPopup(par);
+      // light medium heavy rigid soft
+      this.TWA.HapticFeedback.impactOccurred(heavy);
     },
   }
 }
