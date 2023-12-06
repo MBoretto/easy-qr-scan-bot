3d8f4932d6888ed2a8a3ddf6f3280c652999ee8a
Federico <federico.sangati@gmail.com>
2023-12-06 09:17:43 +0900

    fixe web-app-launcher for PTB > 20

--- web-app-launcher.py
+++ web-app-launcher.py
@@ -16,7 +16,7 @@ Basic example for a bot that uses inline keyboards. For an in-depth explanation,
 import logging
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, WebAppInfo
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, CallbackContext
 
 from config import TOKEN, URL, URL_TEST
 
@@ -26,23 +26,23 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: CallbackContext) -> None:
     """Sends a message with three inline buttons attached."""
     keyboard = [
         [InlineKeyboardButton("Scan QR codes", web_app=WebAppInfo(url=URL))],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Press to launch QR scanner', reply_markup=reply_markup)
+    await update.message.reply_text('Press to launch QR scanner', reply_markup=reply_markup)
 
-def develop(update: Update, context: CallbackContext) -> None:
+async def develop(update: Update, context: CallbackContext) -> None:
     """Sends a message with three inline buttons attached."""
     keyboard = [
         [InlineKeyboardButton("Scan QR codes with develop branch", web_app=WebAppInfo(url=URL_TEST))],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Press to launch QR scanner', reply_markup=reply_markup)
+    await update.message.reply_text('Press to launch QR scanner', reply_markup=reply_markup)
 
-def button(update: Update, context: CallbackContext) -> None:
+async def button(update: Update, context: CallbackContext) -> None:
     """Parses the CallbackQuery and updates the message text."""
     query = update.callback_query
 
@@ -50,30 +50,26 @@ def button(update: Update, context: CallbackContext) -> None:
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
 
-    query.edit_message_text(text=f"Selected option: {query.data}")
+    await query.edit_message_text(text=f"Selected option: {query.data}")
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
+async def help_command(update: Update, context: CallbackContext) -> None:
     """Displays info on how to use the bot."""
-    update.message.reply_text("Type /start and open the QR dialog.")
+    await update.message.reply_text("Type /start and open the QR dialog.")
 
 
 def main() -> None:
     """Run the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater(TOKEN)
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TOKEN).build()
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('dev', develop))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('dev', develop))
+    application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(CommandHandler('help', help_command))
 
     # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT
-    updater.idle()
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == '__main__':
